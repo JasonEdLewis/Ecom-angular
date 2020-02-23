1403da03c83fe65e2850a410da75415b38d9c9cb
jason <jasonedlewis@gmail.com>
2020-02-22 20:26:23 -0600

    added component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,14 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { CoursesComponent } from './courses.compontent';
+import { CourseComponent } from './course/course.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    CoursesComponent
+    CoursesComponent,
+    CourseComponent,
+
 
   ],
   imports: [
