8107de94cdc8d1d83a2c94eda88c11fc40e4e5fc
jason <jasonedlewis@gmail.com>
2020-02-22 22:21:39 -0600

    Added list of courses

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,17 @@
+import { CoursesComponent } from './courses.compontent';
+import { CourseComponent } from './course/course.component';
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { CoursesComponent } from './courses.compontent';
-import { CourseComponent } from './course/course.component';
+
 
 @NgModule({
   declarations: [
     AppComponent,
     CoursesComponent,
     CourseComponent,
-
-
   ],
   imports: [
     BrowserModule,
