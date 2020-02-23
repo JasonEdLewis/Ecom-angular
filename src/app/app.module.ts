8f773c326d15e496e953259e6f029ec331912f51
jason <jasonedlewis@gmail.com>
2020-02-22 19:43:07 -0600

    first from jason

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,13 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { CoursesComponent } from './courses.compontent';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CoursesComponent
+
   ],
   imports: [
     BrowserModule,
